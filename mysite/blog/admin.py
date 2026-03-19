31885178e6ed6167aa592e775e37a5d11795be3f
Kalugin68 <sashakalugin74@gmail.com>
2026-03-19 11:03:50 +0300

    Configure admin panel

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Post
 
 # Register your models here.
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'slug', 'author', 'publish', 'status']     # отображение полей модели
+    list_filter = ['status', 'created', 'publish', 'author']    # Боковая панель фильтрации
+    search_fields = ['title', 'body']   # Поиск по полям
+    prepopulated_fields = {'slug': ('title',)} # Автозаполнение слага по заголовку
+    raw_id_fields = ['author']  # Отображение поля автора поисковым виджетом при добавлении записи
+    date_hierarchy = 'publish'  # Навигация по дате
+    ordering = ['status', 'publish'] # Соритровка по умолчанию
